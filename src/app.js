03c408aa33fee96661ec48f8ffa53a7b6739c498
Rebekah Heal <rebekahheal@rebekahs-mbp.lan>
2022-09-27 21:09:05 -0600

    added contact modal, sends email :)

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function getRandomAnswer() {
 // const disclaimerModal = document.getElementById("disclaimerModal");
 const disclaimerModal = document.getElementsByClassName("disclaimer")[0];
 const discalimerButton = document.getElementById("openDisclaimer");
-const closeDisclaimer = document.getElementsByClassName("close")[0];
+const closeDisclaimer = document.getElementsByClassName("close-disclaimer")[0];
 
 // open disclaimer modal
 discalimerButton.onclick = function() {
@@ -44,4 +44,45 @@ window.onclick = function(event) {
 
 closeDisclaimer.onclick = function() {
     disclaimerModal.style.display = "none";
+}
+
+
+
+// CONTACT CODE //
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "therapyanswers@gmail.com",
+        Password : "secret-password",
+        To : 'therapyanswers@gmail.com',
+        From : 'therapyanswers@gmail.com',
+        Subject : document.getElementById("topic").value,
+        Body : `
+                from: ${document.getElementById("name").value} <br>
+                email: ${document.getElementById("email").value} <br>
+                topic: ${document.getElementById("topic").value} <br>
+                message: <br> ${document.getElementById("message").value}`
+    }).then(
+        message => alert("Your message has been sent. \n Have a joyous day!")
+    );
+}
+
+const contactModal = document.getElementsByClassName("contact")[0]
+const contactButton = document.getElementById("openContact");
+const closeContact = document.getElementsByClassName("close-contact")[0];
+
+// open contact modal
+contactButton.onclick = function() {
+    contactModal.style.display = "block";
+}
+
+// close contact modal
+window.onclick = function(event) {
+    if (event.target == contactModal) {
+        contactModal.style.display = "none";
+    }
+}
+closeContact.onclick = function() {
+    contactModal.style.display = "none";
 }
